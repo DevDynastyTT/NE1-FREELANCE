76d6d39b87c5bb3bd0659f914586f3f368cb1095
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-06-12 03:12:17 -0300

    Converted React Home page to Next.js with bugs

---
+++ nextjs/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import '../../styles/home/index.css';
+import Header from './Header';
+import Main from './Main';
+
+export default function Home() {
+
+  return (
+      <div className="Home">
+          <Header />
+          <Main />
+      </div>
+)}  // End stateless function component

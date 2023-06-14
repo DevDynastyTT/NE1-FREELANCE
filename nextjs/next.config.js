655699af72f786a0e99ec94059295d55d295c4f7
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-06-14 01:39:47 -0300

    Update next.config.js

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -7,9 +7,6 @@ const nextConfig = {
     images: {
       domains: ['lh3.googleusercontent.com', "fiverr-res.cloudinary.com", "localhost:3000"],
     },
-    icons: {
-      icon: '/N.png',
-    },
     webpack(config) {
       config.experiments = {
         ...config.experiments,

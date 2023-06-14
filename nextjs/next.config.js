b86c8ba306080117194f9ecb0b388f322bbcf0d1
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-06-14 01:38:55 -0300

    Replaced Favicon

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     images: {
       domains: ['lh3.googleusercontent.com', "fiverr-res.cloudinary.com", "localhost:3000"],
     },
+    icons: {
+      icon: '/N.png',
+    },
     webpack(config) {
       config.experiments = {
         ...config.experiments,

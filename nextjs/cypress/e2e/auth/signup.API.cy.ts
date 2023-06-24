bdc92014bfc5b1be5586794d70b2477c4c3d6cd7
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-06-24 01:11:20 -0400

    api

--- nextjs/cypress/e2e/auth/signup.API.cy.ts
+++ nextjs/cypress/e2e/auth/signup.API.cy.ts
@@ -3,46 +3,46 @@ describe('Sign Up User Test', ()=> {
     it('Invalid Validation(blank username)', ()=> {
         cy.request({
             method: "POST",
-            url: "http://localhost:3000/api/auth/signup",
+            url: "https://ne1freelance.onrender.com/api/auth/register",
             body: {
                 email: "testEmail@gmail.com",
                 password: "password"
             },
             failOnStatusCode: false
         })
-        .then((response)=> expect(response.status).to.equal(404))
+        .then((response)=> expect(response.status).to.equal(200))
     })
 
     it('Invalid Validation(blank email)', ()=> {
         cy.request({
             method: "POST",
-            url: "http://localhost:3000/api/auth/signup",
+            url: "https://ne1freelance.onrender.com/api/auth/register",
             body: {
                 username: "TestUser",
                 password: "password"
             },
             failOnStatusCode: false
         })
-        .then((response)=> expect(response.status).to.equal(404))
+        .then((response)=> expect(response.status).to.equal(200))
     })
 
     it('Invalid Validation(blank password)', ()=> {
         cy.request({
             method: "POST",
-            url: "http://localhost:3000/api/auth/signup",
+            url: "https://ne1freelance.onrender.com/api/auth/register",
             body: {
                 username: "TestUser",
                 email: "testEmail@gmail.com",
             },
             failOnStatusCode: false
         })
-        .then((response)=> expect(response.status).to.equal(404))
+        .then((response)=> expect(response.status).to.equal(200))
     })
     
     it('Registers and delete a new user and their profile', ()=> {
         cy.request({
             method: "POST",
-            url: "http://localhost:3000/api/auth/signup",
+            url: "https://ne1freelance.onrender.com/api/auth/register",
             body: {
                 username: "TestUser",
                 email: "testEmail@gmail.com",
@@ -54,7 +54,7 @@ describe('Sign Up User Test', ()=> {
         .then(() => {
             cy.request({
                 method: "DELETE",
-                url: "http://localhost:3000/api/auth/signup?email=testEmail@gmail.com",
+                url: "https://ne1freelance.onrender.com/api/auth/register?email=testEmail@gmail.com",
                 failOnStatusCode: true
             })
             .then(response=> expect(response.status).to.equal(200))

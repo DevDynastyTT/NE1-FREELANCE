b8da97bbdde5b49234aa1d3b9086c25cdc33ceeb
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-06-24 02:39:12 -0400

    Updated Title for login and signup

--- nextjs/app/auth/signup/page.tsx
+++ nextjs/app/auth/signup/page.tsx
@@ -5,6 +5,14 @@ import SignUpComponent from '@components/auth/signup/SignUpComponent'
 export default function Signup(){
   const router = useRouter()
 
-  return  <SignUpComponent router={router}/>
+  return  (
+    <>
+      <head>
+        <title>SignUp</title>
+      </head>
+
+      <SignUpComponent router={router}/>
+    </>
+  )
 }
 

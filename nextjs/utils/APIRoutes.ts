e3edb76c636fade43df64c087df064476efe1418
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-06-24 00:15:06 -0400

    updated package.json

--- nextjs/utils/APIRoutes.ts
+++ nextjs/utils/APIRoutes.ts
@@ -1,8 +1,7 @@
 //comment this when deploying
-export const host = "http://localhost:3001"
 
 //Uncomment this whenever deploying publicly
-// export const host = "https://ne1freelance.onrender.com"
+export const host = "http://localhost:3001" || "https://ne1freelance.onrender.com"
 
 export const heartBeat = `${host}/api/heartbeat`
 

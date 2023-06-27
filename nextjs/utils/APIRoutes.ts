7f3af1b61142135b39b30230f1f4b6498679e826
Aaron Hazzard <62683196+A-Hazzard@users.noreply.github.com>
2023-06-26 21:03:35 -0400

    Update APIRoutes.ts

--- nextjs/utils/APIRoutes.ts
+++ nextjs/utils/APIRoutes.ts
@@ -1,8 +1,9 @@
 //comment this when deploying
 
 //Uncomment this whenever deploying publicly
-export const host = "https://ne1freelance.onrender.com"
+// export const host = "https://ne1freelance.onrender.com"
 // export const host = process.env.SERVER_HOST || "http://localhost:3001"
+export const host = process.env.NODE_ENV === 'production' ? process.env.SERVER_HOST : 'http://localhost:3001';
 
 export const heartBeat = `${host}/api/heartbeat`
 
